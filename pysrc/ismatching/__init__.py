07fe25a5079eb79910e64b58fc727e9fb3d52dba
Cael Coonai <149278650+cael-coonai@users.noreply.github.com>
2024-07-11 13:20:51 -0400

    Update ImportanceSampling to store data in _WeightSamplingEntry instances.

--- pysrc/ismatching/__init__.py
+++ pysrc/ismatching/__init__.py
@@ -1,6 +1,6 @@
 from .ismatching import *
 from numpy.typing import NDArray
-from numpy import ceil, std, sqrt, uint8, ndarray, average, float_, random, int_
+import numpy as np
 from scipy.stats import binom, sem
 from typing import Dict, List, Optional, Tuple, Union
 from pymatching import Matching
@@ -8,12 +8,12 @@ from pymatching import Matching
 
 def generate_errors(
     num_qubits: int,
-    num_samples: Union[int,NDArray[int_]]=1,
-    error_rate: Union[float,NDArray[float_]]=0.5,
+    num_samples: Union[int,NDArray[np.uint64]]=1,
+    error_rate: Union[float,NDArray[np.float64]]=0.5,
     num_threads: int=0,
-    error_weight: Optional[Union[int,NDArray[int_]]]=None,
-    rng_seed: Optional[Union[int,NDArray[int_]]]=None,
-) -> NDArray[uint8]:
+    error_weight: Optional[Union[int,NDArray[np.int64]]]=None,
+    rng_seed: Optional[Union[int,NDArray[np.int64]]]=None,
+) -> NDArray[np.uint8]:
     
     return ismatching._generate_errors(
         num_qubits,
@@ -26,13 +26,13 @@ def generate_errors(
 
 
 def generate_errors_from_check(
-    parity_check_matrix: NDArray[uint8],
+    parity_check_matrix: NDArray[np.uint8],
     num_samples: int=1,
     error_rate: float=0.5,
     num_threads: int=0,
     error_weight: Optional[int]=None,
     rng_seed: Optional[int]=None,
-) -> NDArray[uint8]:
+) -> NDArray[np.uint8]:
     num_qubits = parity_check_matrix.shape[1]//2
     return generate_errors(
         num_qubits,
@@ -45,31 +45,31 @@ def generate_errors_from_check(
 
 
 def determine_logical_errors(
-    errors: NDArray[uint8],
-    predictions: NDArray[uint8],
+    errors: NDArray[np.uint8],
+    predictions: NDArray[np.uint8],
     num_threads: int=0 
-) -> NDArray[uint8]:
+) -> NDArray[np.uint8]:
     # TODO: Make safe and documemnt
     return ismatching._determine_logical_errors(errors, predictions, num_threads)
 
 
 def generate_syndromes(
-    parity_check_matrix: NDArray[uint8],
-    errors: NDArray[uint8],
+    parity_check_matrix: NDArray[np.uint8],
+    errors: NDArray[np.uint8],
     num_threads: int=0,
-)-> NDArray[uint8]:
+)-> NDArray[np.uint8]:
     return ismatching._generate_syndromes(parity_check_matrix, errors, num_threads)
 
 
-def get_stats(data: NDArray[uint8]) -> Tuple[float_, float_]:
-    mean = average(data)
-    stddev = std(data, ddof=1)
+def get_stats(data: NDArray[np.uint8]) -> Tuple[np.float64, np.float64]:
+    mean = np.average(data)
+    stddev = np.std(data, ddof=1)
     n = len(data)
-    return (mean, stddev/(sqrt(n)))
+    return (mean, stddev/(np.sqrt(n)))
 
 
 def monte_carlo(
-    parity_check_matrix: NDArray[uint8],
+    parity_check_matrix: NDArray[np.uint8],
     error_rate: float,
     num_samples: int=1,
     num_threads: int=0,
@@ -94,42 +94,47 @@ def monte_carlo(
     )
 
     predictions = matching.decode_batch(syndromes)
-    assert(isinstance(predictions, ndarray))
+    # TODO: Reconsider assertion
+    assert(isinstance(predictions, np.ndarray))
 
     failures = determine_logical_errors(errors,predictions,num_threads)
 
     return get_stats(failures)
 
 
-# class _WeightSamplingEntry:
-#     num_samples: int
-#     stddev: float
-#     mean: float
+class _WeightSamplingEntry:
+    num_samples: np.uint64
+    mean: np.float64
+    stddev: np.float64
 
-#     def __init__(self, num_samples:int, stddev:float, mean:float):
-#         self.num_samples = num_samples
-#         self.stddev = stddev
-#         self.mean = mean
+    def __init__(self, num_samples:np.uint64, mean:np.float64, stddev:np.float64):
+        self.num_samples = num_samples
+        self.mean = mean
+        self.stddev = stddev
 
-#     def add_data(self, num_samples:int, stddev:float, mean:float) -> None:
-#         self.mean = (self.num_samples*self.mean + num_samples*mean) \
-#                     / (self.num_samples+num_samples)
-#         self.stddev = sqrt(((self.num_samples-1)*(self.stddev**2) \
-#                             + (num_samples-1)*(stddev**2)) \
-#                             / (self.num_samples+num_samples-1))
-#         self.num_samples = self.num_samples + num_samples
+    @staticmethod
+    def zero():
+        return _WeightSamplingEntry(np.uint64(0), np.float64(0), np.float64(0))
 
-#     def guess_num_samples_difference(self, stderr_target: float) -> int:
-#         total_num_samples = ceil((self.stddev / stderr_target)**2)
-#         diff_num_samples = total_num_samples - self.num_samples
-#         return diff_num_samples if diff_num_samples > 0 else 0
+    def add_data(self, num_samples:np.uint64, mean:np.float64, stddev:np.float64) -> None:
+        self.mean = (self.num_samples*self.mean + num_samples*mean) \
+                    / (self.num_samples+num_samples)
+        self.stddev = np.sqrt(((self.num_samples-1)*(self.stddev**2) \
+                            + (num_samples-1)*(stddev**2)) \
+                            / (self.num_samples+num_samples-1))
+        self.num_samples = self.num_samples + num_samples
+
+    def guess_num_samples_difference(self, stderr_target: np.float64) -> np.uint64:
+        total_num_samples = np.ceil((self.stddev / stderr_target)**2)
+        diff_num_samples = total_num_samples - self.num_samples
+        return diff_num_samples if diff_num_samples > 0 else np.uint64(0)
 
 
-#     def stats(self) -> Tuple[float, float]:
-#         """
-#         Returns a tuple containing the mean and standard error.
-#         """
-#         return (self.mean, self.stddev/sqrt(self.num_samples))
+    def stats(self) -> Tuple[np.float64, np.float64]:
+        """
+        Returns a tuple containing the mean and standard error.
+        """
+        return (self.mean, self.stddev/np.sqrt(self.num_samples))
 
 class ImportanceSampling:
     """
@@ -139,28 +144,23 @@ class ImportanceSampling:
 
     def __init__(
         self,
-        parity_check_matrix: NDArray[uint8],
+        parity_check_matrix: NDArray[np.uint8],
         # initial_num_samples: int,
         code_distance: Optional[int]=None,
         matching: Optional[Matching]=None,
         num_threads: int=0,
-        rng_seed: Optional[int]=None,
+        rng_seed: Optional[int]=None, # TODO: Replace to allow input of RNG directly
     ):
         self._matching = Matching(parity_check_matrix) \
             if matching == None else matching
         self._parity_check_matrix = parity_check_matrix
         self.num_threads = num_threads
-        self._rng = random.default_rng(rng_seed)
-        # self._data: Dict[int, _WeightSamplingEntry] = dict()
-        # self._data[0] = _WeightSamplingEntry(0,0,0)
-        # if code_distance != None:
-        #     for weight in range(code_distance//2):
-        #         self._data[weight] = _WeightSamplingEntry(0,0,0)
-        self.p_log_by_weight: Dict[int, Tuple[float,float]] = dict()
-        self.p_log_by_weight[0] = (0,0)
+        self._rng = np.random.default_rng(rng_seed)
+        self._data: Dict[int, _WeightSamplingEntry] = dict()
+        self._data[0] = _WeightSamplingEntry.zero()
         if code_distance != None:
             for weight in range(code_distance//2):
-                self.p_log_by_weight[weight] = (0,0)
+                self._data[weight] = _WeightSamplingEntry.zero()
 
 
     def num_qubits(self) -> int:
@@ -173,6 +173,9 @@ class ImportanceSampling:
         num_samples: int,
         threshold_err: Optional[float]=None
     ) -> None:
+
+        num_samples = np.uint64(num_samples)
+
         errors = generate_errors(
             self.num_qubits(),
             num_samples,
@@ -188,11 +191,25 @@ class ImportanceSampling:
         )
 
         predictions = self._matching.decode_batch(syndromes)
-        assert(isinstance(predictions,ndarray))
+        assert(isinstance(predictions, np.ndarray))
 
         failures = determine_logical_errors(errors,predictions,self.num_threads)
 
-        self.p_log_by_weight[error_weight] = get_stats(failures)
+        self._data[error_weight] = _WeightSamplingEntry(
+            num_samples,
+            np.average(failures),
+            np.std(failures, ddof=1)
+        )
+    
+    def get_weights(self) -> List[Tuple[int, Tuple[np.float64, np.float64]]]:
+        """
+        Each tuple is: (weight, (mean, standard error))
+        """
+        weights: List[Tuple[int, Tuple[np.float64, np.float64]]] = []
+        for w, e in self._data.items():
+            weights.append((w, e.stats()))
+        return weights
+
 
 
     def _get_p_log_bound_by_weight(
@@ -201,21 +218,21 @@ class ImportanceSampling:
             p_phys: float,
             num_samples: int,
             weight_prob_threshold: Optional[float],
-     ) -> Tuple[Tuple[float,float], float]:
+     ) -> Tuple[Tuple[np.float64, np.float64], np.float64]:
 
-        if weight in self.p_log_by_weight.keys():
-            p_log, p_log_err = self.p_log_by_weight[weight]
+        if weight in self._data.keys():
+            p_log, p_log_err = self._data[weight].stats()
             return ((p_log,p_log),p_log_err)
 
         elif weight_prob_threshold != None and (
                 binom.pmf(weight,self.num_qubits(),p_phys)>=weight_prob_threshold
             ).any():
             self.add_weight(weight, num_samples)
-            p_log, p_log_err = self.p_log_by_weight[weight]
+            p_log, p_log_err = self._data[weight].stats()
             return ((p_log,p_log),p_log_err)
 
         else:
-            return ((0,1),0)
+            return ((np.float64(0), np.float64(1)), np.float64())
 
 
     def p_log_bound(
@@ -236,6 +253,6 @@ class ImportanceSampling:
             p_log_min += binom.pmf(weight,self.num_qubits(),p_phys) * low
             p_log_max += binom.pmf(weight,self.num_qubits(),p_phys) * high
             p_log_err_to2+=(binom.pmf(weight,self.num_qubits(),p_phys) * err)**2
-        p_log_err = sqrt(p_log_err_to2)
+        p_log_err = np.sqrt(p_log_err_to2)
         return ((p_log_min, p_log_max), p_log_err)
 
