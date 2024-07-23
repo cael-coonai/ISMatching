7a4813bd72a96cdeb44c33206c955d7ca86db758
Cael Coonai <149278650+cael-coonai@users.noreply.github.com>
2024-07-23 13:43:56 -0400

    Removed * and added __all__.

--- pysrc/ismatching/__init__.py
+++ pysrc/ismatching/__init__.py
@@ -1,2 +1,15 @@
-from ismatching.is_functions import *
-from ismatching.is_functions import *
+from ismatching.is_functions import (
+    generate_errors,
+    generate_errors_from_check,
+    determine_logical_errors,
+    monte_carlo
+)
+from ismatching.is_class import ImportanceSampling
+
+__all__ = [
+    "generate_errors",
+    "generate_errors_from_check",
+    "determine_logical_errors",
+    "monte_carlo",
+    "ImportanceSampling"
+]
