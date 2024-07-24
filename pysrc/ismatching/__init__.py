6d977437d245423e350c1e1c26e03c12430a6037
Cael Coonai <149278650+cael-coonai@users.noreply.github.com>
2024-07-24 14:29:59 -0400

    Removed error generation from check from __all__

--- pysrc/ismatching/__init__.py
+++ pysrc/ismatching/__init__.py
@@ -8,7 +8,7 @@ from ismatching.pyclass import ImportanceSampling
 
 __all__ = [
     "generate_errors",
-    "generate_errors_from_check",
+    # "generate_errors_from_check",
     "determine_logical_errors",
     "monte_carlo",
     "ImportanceSampling"
